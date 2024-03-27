5fd1321505866bb1ac69c7eb174e89769371eab4
dabour1 <Aldabor63@Yahoo.Com>
2024-03-27 14:42:03 +0200

    ...

--- Routes/childRouter.js
+++ Routes/childRouter.js
@@ -4,23 +4,22 @@ const controller = require("../Controller/childController");
 const {
   insertValidator,
   updateValidator
-  ,deleteGetOneValidator
+  , deleteGetOneValidator
 } = require("./../Midelwares/validations/childValidation");
 const validatonResult = require("../Midelwares/validations/validatorResult");
-const { isAdmin,isAuthorizatied } = require("./../Midelwares/authenticationMW");
+const { isAdmin, isAuthorizatied } = require("./../Midelwares/authenticationMW");
 
-
+const upload = require('../Controller/upload');
 
 router
   .route("/child")
-  .get( isAuthorizatied ,controller.getAllChilds)
-  .post(isAdmin,insertValidator, validatonResult,controller.insertChild)
-  .patch(isAdmin,updateValidator,validatonResult,controller.updateChild);
-  router
+  .get(isAuthorizatied, controller.getAllChilds)
+  .post(isAdmin, upload.single("user"), insertValidator, validatonResult, controller.insertChild)
+  .patch(isAdmin, upload.single("user"), updateValidator, validatonResult, controller.updateChild);
+router
   .route("/child/:_id")
-  .get(isAuthorizatied,deleteGetOneValidator,validatonResult,controller.getChildById)
-  .delete(isAdmin, deleteGetOneValidator,validatonResult, controller.deleteChildById);
- 
+  .get(isAuthorizatied, deleteGetOneValidator, validatonResult, controller.getChildById)
+  .delete(isAdmin, deleteGetOneValidator, validatonResult, controller.deleteChildById);
+
 module.exports = router;
 
- 
